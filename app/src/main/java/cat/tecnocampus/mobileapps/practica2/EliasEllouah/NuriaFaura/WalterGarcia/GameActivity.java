45a197a9e05ca9a7653803c1ffc96839ce0b1a7b
eellouah <eellouah@edu.tecnocampus.cat>
2022-05-20 19:24:42 +0200

    commit

---
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/EliasEllouah/NuriaFaura/WalterGarcia/GameActivity.java
@@ -0,0 +1,81 @@
+package cat.tecnocampus.mobileapps.practica2.EliasEllouah.NuriaFaura.WalterGarcia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class GameActivity extends AppCompatActivity {
+    TextView textView;
+    int letterSize;
+    String replace="";
+    String paraula[];
+    String ancerts [];
+    String initial="";
+    EditText editText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game);
+
+        textView = findViewById(R.id.randomWord);
+        editText = findViewById(R.id.lletra);
+
+        String text = getIntent().getStringExtra("sendWord");
+        letterSize = text.length();
+        paraula = new String[text.length()];
+        ancerts = new String[text.length()];
+        for (int i = 0; i< letterSize;i++) {
+            ancerts[i] = " _";
+            paraula[i] = text.substring(i,i+1);
+            System.out.println("la paraula es: "+paraula[i]);
+            initial = initial+" _";
+
+        }
+
+
+        textView.setText(initial);
+
+    }
+
+    public void searchChar(View view) {
+        String letter = editText.getText().toString();
+        if (letter == null || letter.equals("")) {
+            //fer TOAST
+            System.out.println("escriu alguna cosa");
+        }
+        else {
+
+            for (int i = 0; i<letterSize; i++) {
+                System.out.println(paraula[i]);
+            }
+
+            for (int i = 0; i<letterSize;i++) {
+                System.out.println("abans de comparar lletra es "+letter+ " caixa es: "+paraula[i]);
+                if (paraula[i].equals(letter)) {
+                    ancerts[i] = letter;
+                    //textView.setText(ancerts.toString());
+                    i = letterSize;
+
+                    for (int r = 0; r < letterSize;r++) {
+                        String t = "";
+                        replace = replace + ancerts[r];
+
+                    }
+                    textView.setText(replace);
+                    replace = "";
+                }
+
+                else {
+                    //sonido de error
+                }
+            }
+
+        }
+
+    }
+}
--- app/src/main/java/cat/tecnocampus/mobileapps/practica2/EliasEllouah/NuriaFaura/WalterGarcia/MainActivity.java
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/EliasEllouah/NuriaFaura/WalterGarcia/MainActivity.java
@@ -1,17 +1,39 @@
 package cat.tecnocampus.mobileapps.practica2.EliasEllouah.NuriaFaura.WalterGarcia;
 
+import android.content.Intent;
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
 
     MediaPlayer mp;
+    EditText editText;
+    RequestQueue queue; // Declara
+    String url = "https://palabras-aleatorias-public-api.herokuapp.com/random";
+    String url2 = "https://random-word-api.herokuapp.com/word";
+    String random;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        queue = Volley.newRequestQueue(getApplicationContext());
 
         if (mp != null) {
             mp.release();
@@ -20,6 +42,42 @@ public class MainActivity extends AppCompatActivity {
         mp = MediaPlayer.create(this,R.raw.ambient_5);
         mp.seekTo(1);
         mp.setLooping(true);
-        mp.start();
+        //mp.start();
+
+        editText = findViewById(R.id.nickname_txt);
+
+        StringRequest stringRequest = new StringRequest(
+                Request.Method.GET, url2, new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                //Aqui ens arriba la resposta de la request que estem fent
+                Log.v("TEST", response);
+                random = response;
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("TEST", "Error al StringRequest");
+
+            }
+        }
+
+        );
+        queue.add(stringRequest);
+    }
+
+
+    public void startGame(View view) {
+
+
+        random = random.replaceAll("\\W+","");
+        String nickname = editText.getText().toString();
+        Intent intent = new Intent(getApplicationContext(),GameActivity.class);
+        intent.putExtra("sendNickName",nickname);
+        System.out.println("Palabra que queremos enviar: "+random);
+        intent.putExtra("sendWord",random);
+        startActivity(intent);
+
     }
 }

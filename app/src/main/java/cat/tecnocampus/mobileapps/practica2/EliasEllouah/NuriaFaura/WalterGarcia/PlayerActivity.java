cc54e4acce22231a4154fe602b374df350c71846
eellouah <eellouah@edu.tecnocampus.cat>
2022-05-27 11:07:16 +0200

    player Activity

---
+++ app/src/main/java/cat/tecnocampus/mobileapps/practica2/EliasEllouah/NuriaFaura/WalterGarcia/PlayerActivity.java
@@ -0,0 +1,14 @@
+package cat.tecnocampus.mobileapps.practica2.EliasEllouah.NuriaFaura.WalterGarcia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlayerActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_player);
+    }
+}
